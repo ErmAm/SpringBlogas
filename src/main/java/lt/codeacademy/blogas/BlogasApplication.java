7db1e0f5f45d876edca3e7dc8cf8916c4d2c28f9
ErmAm <erminas.ambrasas@gmail.com>
2021-04-25 19:06:22 +0300

    Pirmas komitas pakurto projekto

---
+++ src/main/java/lt/codeacademy/blogas/BlogasApplication.java
@@ -0,0 +1,12 @@
+package lt.codeacademy.blogas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogasApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BlogasApplication.class, args);
+	}
+
+}
