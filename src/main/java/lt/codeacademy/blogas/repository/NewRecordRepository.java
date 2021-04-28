76822edfae0f1b2e588aaf42170045a048723615
ErmAm <erminas.ambrasas@gmail.com>
2021-04-29 01:02:25 +0300

    užsiroviau ant jpa implimentacijos, bet jau aiskėja ka dartyti

--- src/main/java/lt/codeacademy/blogas/repository/NewRecordRepository.java
+++ src/main/java/lt/codeacademy/blogas/repository/NewRecordRepository.java
@@ -1,4 +1,15 @@
 package lt.codeacademy.blogas.repository;
 
-public interface NewRecordRepository {
+import lt.codeacademy.blogas.model.BlogRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface JPANewRecordRepository extends JpaRepository<BlogRecord, UUID> {
+
+//    @Override
+//    default List<BlogRecord> findAll() {
+//        return null;
+//    }
 }
