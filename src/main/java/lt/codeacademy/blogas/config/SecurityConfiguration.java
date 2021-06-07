e808db203a61a915f6f094b64389126245940e6e
ErmAm <erminas.ambrasas@gmail.com>
2021-06-07 15:04:19 +0300

    Tvarkausi login puslpaį.

--- src/main/java/lt/codeacademy/blogas/config/SecurityConfiguration.java
+++ src/main/java/lt/codeacademy/blogas/config/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package lt.codeacademy.blogas.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -12,8 +11,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 //    Praleidžiam visą srautą
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/**");
+    public void configure(HttpSecurity http) throws Exception {
+        http.
+                authorizeRequests()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                .formLogin()
+                    .permitAll()
+                    .loginPage("/login")
+                    .loginProcessingUrl("/login")
+
+                    .usernameParameter("loginName")
+                    .passwordParameter("loginPassword")
+
+                    .defaultSuccessUrl("/records/all",true)
+                    .failureUrl("/login?error=true")
+                    .and()
+                .logout()
+                    .permitAll()
+        ;
+
     }
 
 
--- src/main/java/lt/codeacademy/blogas/controller/LoginController.java
+++ src/main/java/lt/codeacademy/blogas/controller/LoginController.java
@@ -8,13 +8,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class LoginController {
 
-// ==   Pridedu testavimo metodą pasitikrinti ar važiuoja saitas
 
-    @GetMapping("/{text}")
-    public String hello(@PathVariable String text, Model model){
-
-//        reikia returninti templeito adresą.
-        return "hello";
+    @GetMapping("/login")
+    public String hello() {
+        return "login";
     }
+
 
 }
